42a69f563b5e0a3973a3a0874249b27260508335
jacklv999 <jacklv64426@hotmail.com>
2021-09-14 13:13:07 +0800

    add

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,26 @@
-import os, json, re, os, math, cv2
-from flask import Flask, request, send_from_directory
+import os, json, re, os, math, cv2, base64
+from flask import Flask, request, send_from_directory, jsonify
 from flask_cors import CORS
 import numpy as np
 
 
 
-def padding_pic_citrus(img):
+def padding_pic(img):
     img_row_num = 3 - img.shape[0]%3
     img=cv2.copyMakeBorder(img, 0, img_row_num, 0, 0, cv2.BORDER_CONSTANT, value=0)
     return img
 
-def pest_count_by_kmeans_and_binarz_citrus(Img, num_clusters=2):
-    #Img = cv2.imread(path, 1)
+def pest_count_by_kmeans_and_binarz(Img, kernel_size, binary_method, num_clusters=2):
     Img = Img[:,:,0]
     # cv2.GaussianBlur
-    blur = cv2.GaussianBlur(Img, (1, 1), 0)
+    blur = cv2.GaussianBlur(Img, (kernel_size, kernel_size), 0)
     
     # binarization
-    ret, otsu = cv2.threshold(blur, 90, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    print(ret)
+    if binary_method == 1:
+        ret, otsu = cv2.threshold(blur, 90, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    else: 
+        _, otsu = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY)
+    
     
     # k-means
     data = otsu.reshape((-1,3))
@@ -33,11 +35,9 @@ def pest_count_by_kmeans_and_binarz_citrus(Img, num_clusters=2):
     color = np.uint8([[255, 0, 0],[128, 128, 128]])
     res = color[label.flatten()]
     result = res.reshape((Img.shape))
-    #src = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
-    #plt.imshow(src),plt.xticks([]),plt.yticks([]),plt.show()
     return result
 
-def Elliptical_Fit_citrus(kmeans_result, original_file):
+def Elliptical_Fit(kmeans_result, original_file):
     blur = cv2.GaussianBlur(kmeans_result, (9, 9), 0)
     res, otsu = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     # cv2.Canny input three value: source img, min value, max value
@@ -56,17 +56,24 @@ def Elliptical_Fit_citrus(kmeans_result, original_file):
                            (np.int32(a/2), np.int32(b/2)), angle, 0, 360, (0, 0, 255), 1, 8)
                 ellipse_area.append(round(math.pi*a*b/4,3))
     
-    cv2.imwrite("citrus_res.png",original_file)
+    cv2.imwrite("res.png",original_file)
     src = cv2.cvtColor(original_file, cv2.COLOR_BGR2RGB)
     return (original_file, ellipse_area)
 
 def Thrips_Counting(path):
     croped_img = cv2.imread(path)
-    img = padding_pic_citrus(croped_img)
-    kmeans_result = pest_count_by_kmeans_and_binarz_citrus(img)
-    Elliptical_Fit_result, ellipse_area = Elliptical_Fit_citrus(kmeans_result, croped_img)
+    img = padding_pic(croped_img)
+    kmeans_result = pest_count_by_kmeans_and_binarz(img, kernel_size=1, binary_method=1)
+    Elliptical_Fit_result, ellipse_area = Elliptical_Fit(kmeans_result, croped_img)
     return Elliptical_Fit_result, ellipse_area
 
+def whitefly_counting(path):
+    #croped_img = sticky_card_crop(path)
+    croped_img = cv2.imread(path)
+    img = padding_pic(croped_img)
+    kmeans_result = pest_count_by_kmeans_and_binarz(img, kernel_size=9, binary_method=2)
+    Elliptical_Fit_result, ellipse_area = Elliptical_Fit(kmeans_result, croped_img)
+    return Elliptical_Fit_result, ellipse_area
 
 app = Flask(__name__)
 CORS(app)
@@ -77,8 +84,20 @@ def pestcounting_thrips_api(pic_name):
     path = "./data/" + pic_name
     Elliptical_Fit_result, ellipse_area = Thrips_Counting(path)
     res = len(ellipse_area)
-    return (str(res))
+    base64_str = cv2.imencode(".jpg",Elliptical_Fit_result)[1].tobytes()
+    base64_str = base64.b64encode(base64_str)
+    return jsonify({'res_num': res, 'res_pic': str(base64_str)[2:-2]})#del b'
 
+
+@app.route('/pestcounting_api/thrips/upload/',methods=['post'])
+def pestcounting_thrips():
+    img = request.files.get('pic')
+    img.save("./test.jpg")
+    Elliptical_Fit_result, ellipse_area = Thrips_Counting("./test.jpg")
+    res = len(ellipse_area)
+    base64_str = cv2.imencode(".jpg",Elliptical_Fit_result)[1].tobytes()
+    base64_str = base64.b64encode(base64_str)
+    return jsonify({'res_num': res, 'res_pic': str(base64_str)[2:-2]})
 
 @app.route('/pestcounting_api/whitefly/<pic_name>',methods=['get'])
 def pestcounting_whitefly_api(pic_name):
@@ -87,6 +106,16 @@ def pestcounting_whitefly_api(pic_name):
     index = src_lst.index(pic_name)
     res = res_num[index]
     return (str(res))
+
+@app.route('/pestcounting_api/whitefly/upload/',methods=['post'])
+def pestcounting_whitefly():
+    img = request.files.get('pic')
+    img.save("./test.jpg")
+    Elliptical_Fit_result, ellipse_area = Thrips_Counting("./test.jpg")
+    res = len(ellipse_area)
+    base64_str = cv2.imencode(".jpg",Elliptical_Fit_result)[1].tobytes()
+    base64_str = base64.b64encode(base64_str)
+    return jsonify({'res_num': res, 'res_pic': str(base64_str)[2:-2]})
 
 @app.route('/weather_api/',methods=['get'])
 def weather_api():
@@ -109,4 +138,4 @@ def location_api(location):
     return (result[0])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=900)
+    app.run(host='0.0.0.0',port=9000)
