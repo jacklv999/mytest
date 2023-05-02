87dd06e98742b002b5ec56e9c1846eab778ded1b
jacklv999 <jacklv64426@hotmail.com>
2023-05-02 18:14:04 +0800

    add

--- 读书笔记/投资相关/量化投资/md.py
+++ 读书笔记/投资相关/量化投资/md.py
@@ -1,11 +1,11 @@
 import os
 
 if not os.path.exists('md.db'):
-    with open('md.db','w') as f:
+    with open('md.db','w',encoding='UTF-8') as f:
         for fs in os.listdir():
             f.write(fs+'\n')
 else:
-    with open('./md.db','r') as f:
+    with open('./md.db','r',encoding='UTF-8') as f:
         file_list = f.read().splitlines()
 
 
@@ -16,17 +16,17 @@ for item in os.listdir():
         add_items.append(item)
 
 
-with open("./README.md", "a") as f:
+with open("./README.md", "a",,encoding='UTF-8') as f:
     for item in add_items:
         if item[-4:] == 'html':
             tmp_str = "- [" + item[:-5] + "](./" + item + ") \n"
             f.write(tmp_str)
         
-with open('./md.db', "r+") as f:
+with open('./md.db', "r+",encoding='UTF-8') as f:
         read_data = f.read()
         f.seek(0)
         f.truncate()   #清空文件
 
-with open('./md.db','a') as f:
+with open('./md.db','a',encoding='UTF-8') as f:
     for file in os.listdir():
         f.write(str(file)+'\n')
