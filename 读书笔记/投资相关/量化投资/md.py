a9980e00f16e970d5302d661f741ba7eecaaf4f0
jacklv999 <jacklv64426@hotmail.com>
2023-05-02 16:37:34 +0800

    add

---
+++ 读书笔记/投资相关/量化投资/md.py
@@ -0,0 +1,33 @@
+import os
+
+if not os.path.exists('md.db'):
+    with open('md.db','w') as f:
+        for fs in os.listdir():
+            f.write(fs)
+            f.write('\n')
+else:
+    with open('./md.db','r') as f:
+        file_list = f.read().splitlines()
+
+
+add_items = []
+
+for item in os.listdir():
+    if item not in file_list:
+        add_items.append(item)
+
+
+with open("./README.md", "a") as f:
+    for item in add_items:
+        if item[-4:] == 'html':
+            tmp_str = "- [" + item[:-5] + "](./" + item + ") \n"
+            f.write(tmp_str)
+        
+with open('./md.db', "r+") as f:
+        read_data = f.read()
+        f.seek(0)
+        f.truncate()   #清空文件
+
+with open('./md.db','a') as f:
+    for file in os.listdir():
+        f.write(str(file)+'\n')
